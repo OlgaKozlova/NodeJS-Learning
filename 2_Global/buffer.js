41dc341379b77029c2bce69de579784e09fd4f89
Olga Kozlova <olgakozlova@coherentsolutions.com>
2017-09-28 15:03:36 +0300

    Timeouts added

--- 2_Global/buffer.js
+++ 2_Global/buffer.js
@@ -1,38 +1,97 @@
-function createBuffer() {
-    console.group('Buffer.from(array) tests');
-    var array = [0x62, 0x75, 0x66, 0x66, 0x65, 0x72, 0xFF];
-    console.log('Here is my array', array);
-    const buf = Buffer.from(array);
-    console.log('Buffer from array: ', buf);
-    array[0] = 0x01;
-    console.log('1st element of array changed to be 0x01');
-    console.log('But buffer is not changed, this is a copy', buf);
-    console.groupEnd('Buffer.from(array) tests');
-
-    const buffer = new ArrayBuffer(8);
-    const view = new Int8Array(buffer);
-    for (let i = 0; i < 8; i++) {
-        view[i] = i + 1;
-    }
-
-    const buf3 = Buffer.from(buffer);
-    console.log(buf3);
-    const buf4 = Buffer.from(buffer, 2, 2);
-    console.log(buf4);
-    view[0] = 0;
-    console.log(buf3);
-
-    const buf5 = Buffer.from('buffer');
-    console.log(buf5);
-
-    const buf6 = Buffer.from(buf5);
-    console.log(buf6);
-
-    buf5[0] = 0;
-    console.log(buf5);
-    console.log(buf6);
-
-
+function createBufferFromArray() {
+    // Buffer from Array of 0x numbers
+    const sourceArray = [0x62, 0x75, 0x66, 0x66, 0x65, 0x72, 0xFF];
+    console.log('Here is my array', sourceArray);
+    const bufferFromArray = Buffer.from(sourceArray);
+    console.log('Here is buffer from array: ', bufferFromArray);
+    sourceArray[0] = 0x01;
+    console.log('1st element of array changed to be ', sourceArray[0]);
+    console.log('But buffer is not changed, this is a copy:', bufferFromArray);
+    // Buffer from Array of non 0x numbers
+    const sourceArray2 = [1 ,2, 3, 16, 18];
+    console.log('Here is Buffer from non 16-numberic array', Buffer.from(sourceArray2));
+    // Buffer from mixed array
+    const sourceArray3 = [{name: 'Olga'}, [6], 'Olga Kozlova', '2.2', null, undefined ];
+    console.log('Here is Buffer mixed affay', Buffer.from(sourceArray3));
+    // Buffer from Array-like object
+    const sourceArray4 = {length: 5};
+    console.log('Here is Buffer from array-like object', Buffer.from(sourceArray4));
 }
 
-createBuffer();
+function createBufferFromArrayBuffer() {
+    // создаем и заполняем ArrayBuffer;
+    const sourceArrayBuffer = new ArrayBuffer(8);
+    const view = new Int8Array(sourceArrayBuffer);
+    for (let i = 0; i < 8; i++) {
+        view[i] = i + 10;
+    }
+
+    // cоздаем Buffer из всего ArrayBuffer
+    const buffer1 = Buffer.from(sourceArrayBuffer);
+    console.log(buffer1);
+    // создаем Buffer из кусочка ArrayBuffer
+    const buffer2 = Buffer.from(sourceArrayBuffer, 2, 2);
+    console.log(buffer2);
+    // меняем исходный ArrayBuffer
+    view[3] = 0;
+    console.log(buffer1);
+    console.log(buffer2);
+}
+
+function createBufferFromString() {
+    const buf1 = Buffer.from('this is a tést');
+    const buf2 = Buffer.from('this is a tést', 'ascii');
+    const buf3 = Buffer.from('7468697320697320612074c3a97374', 'hex');
+    console.log(buf1.toString());
+    console.log(buf2.toString());
+    console.log(buf3.toString());
+    const buf4 = Buffer.from('7468697320697320612074c3a97374', 'hexq');
+}
+
+function createBufferFromAnotherBuffer() {
+    const buf1 = Buffer.from('buffer');
+    const buf2 = Buffer.from(buf1);
+    buf1[0] = 0x61;
+    console.log(buf1.toString());
+    console.log(buf2.toString());
+}
+
+function createBufferFromObject() {
+    const buf = Buffer.from(new String('this is a test'));
+    console.log(buf);
+    //const buf1 = Buffer.from(new Number(5));
+    //console.log(buf1);
+
+    class Foo {
+        [Symbol.toPrimitive]() {
+            return 'this is a test';
+        }
+    }
+    const buf3 = Buffer.from(new Foo(), 'utf8');
+    console.log(buf3);
+    class Foo2 {
+        [Symbol.toPrimitive]() {
+            return '';
+        }
+        valueOf() {return [1,2,3] }
+    }
+    const buf4 = Buffer.from(new Foo2(), 'utf8');
+    console.log(buf4);
+}
+
+function BufferFromOtherArgumentsCausesError() {
+    // Other arguments cause errors
+    try {
+        Buffer.from({});
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+
+// createBufferFromArray();
+// createBufferFromArrayBuffer();
+// createBufferFromString();
+// createBufferFromAnotherBuffer();
+createBufferFromObject();
+//BufferFromOtherArgumentsCausesError()
---
+++ 2_Global/timeouts.js
@@ -0,0 +1,71 @@
+function setTimeoutTest() {
+    const timeout1 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 2000, 'one1', 'two1');
+    const timeout2 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 5000, 'one2', 'two2');
+
+    clearTimeout(timeout2);
+}
+
+function setIntervalTest() {
+    let counter = 0;
+    const interval = setInterval((one, two) => {
+        counter++;
+        console.log(one, two);
+        if (counter >= 5) {
+            clearInterval(interval);
+        }
+    }, 1000, 'one1', 'two1');
+}
+
+function mixedTests1() {
+    const timeout1 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 2000, 'one1', 'two1');
+    const timeout2 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 5000, 'one2', 'two2');
+
+    clearInterval(timeout2);
+}
+
+function mixedTests2() {
+    let counter = 0;
+    const interval = setInterval((one, two) => {
+        counter++;
+        console.log(one, two);
+        if (counter >= 5) {
+            clearTimeout(interval);
+        }
+    }, 1000, 'one1', 'two1');
+}
+
+function refUnrefTests() {
+    const timeout1 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 2000, 'one1', 'two1');
+    const timeout2 = setTimeout((one, two) => {
+        console.log(one, two)
+    }, 5000, 'one2', 'two2');
+    timeout2.unref();
+}
+
+function setImmediateTest() {
+    const immediate = setImmediate((one, two) => {
+        console.log(one, two);
+    }, 'one', 'two');
+    clearImmediate(immediate);
+
+    const immediate2 = setImmediate((one, two) => {
+        console.log(one, two);
+    }, 'one2', 'two2');
+}
+
+// setTimeoutTest();
+// setIntervalTest();
+// mixedTests1();
+// mixedTests2();
+// refUnrefTests();
+ setImmediateTest();
